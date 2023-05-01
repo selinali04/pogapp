fb08cd71f0413a1d6d55e2d05be162b7e53f31d3
selinali04 <124477271+selinali04@users.noreply.github.com>
2023-04-30 23:30:35 -0400

    Add files via upload

---
+++ pages/welcome/welcome.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import logo from '/Users/selinali/Downloads/pogwebsite/pogapp/asset/POGS_2.png';
+import classNames from "classnames";
+
+const Welcome = () => {
+  return (
+    <div className={classNames(
+      "p-1.5 pb-0"
+    )}>
+      <Image src={logo} alt="My Image" width={100} overflow= 'hidden' />
+    </div>
+  );
+};
+
+export default Welcome;
